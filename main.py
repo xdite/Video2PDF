287b1f77fa539711fe833a2677e1d9360cbbea45
xdite <xdite@hey.com>
2023-07-06 09:08:10 +0800

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from transcript import process_video_subs
+from book import convert_png_to_pdf
 
 def main():
     # 取得檔名
@@ -8,7 +9,13 @@ def main():
         exit()
 
     video_name = sys.argv[1]
+
+    # Process video subs and create screenshots
     process_video_subs(video_name)
+
+    # Convert screenshots to PDF
+    base_name = os.path.splitext(video_name)[0]
+    convert_png_to_pdf(base_name, base_name)
 
 if __name__ == "__main__":
     main()
