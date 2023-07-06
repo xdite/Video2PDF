ae73bde812ffe7d3993cae5924ebdaa9a8d1dad3
xdite <xdite@hey.com>
2023-07-06 11:56:58 +0800

    進行寬度 scale

--- video_to_images.py
+++ video_to_images.py
@@ -29,10 +29,20 @@ def video_to_images(video_file_name: str):
             end_time = int(end_time[0])*3600 + int(end_time[1])*60 + float(end_time[2].replace(",", "."))
             text = " ".join(parts[2:])
             frame_height = clip.size[1]
+            # Get video width
+            frame_width = clip.size[0]
+
+            # Calculate scale factor
+            scale_factor = frame_width / 1920
+
+            # Calculate new text position and size
+            text_size = 48 * scale_factor
             text_pos = ('center', frame_height - 100)
 
-            text_clip = TextClip(text, font="黑體-簡-中黑", fontsize=48, color='yellow', stroke_color ="black", stroke_width=0.5).set_duration(end_time - start_time).set_position(text_pos)
-
+            # Create TextClip with new size and position
+            text_clip = (TextClip(text, font="黑體-簡-中黑", fontsize=text_size, color='yellow', stroke_color ="black", stroke_width=0.5*scale_factor)
+                .set_duration(end_time - start_time)
+                .set_position(text_pos))
             frame = clip.get_frame(start_time)
             frame_clip = ImageClip(frame).set_duration(end_time - start_time)
             final = CompositeVideoClip([frame_clip, text_clip])
