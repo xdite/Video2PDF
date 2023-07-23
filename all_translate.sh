b8a3fc1d023390551c0b37738fdec9bb90189345
xdite <xdite@hey.com>
2023-07-23 13:12:59 +0800

    Fix sh

--- all_translate.sh
+++ all_translate.sh
@@ -3,8 +3,16 @@
 # get list of srt files
 for i in *.srt
 do
+  # Get the base name without extension
+  base_name=$(basename "$i" .srt)
+
+  # Check if this is already a .zh.srt file, if so, skip it
+  if [[ $base_name == *.zh ]]; then
+    continue
+  fi
+
   # check if corresponding .zh.srt exists
-  zh_srt_name="${i%.*}.zh.srt"
+  zh_srt_name="${base_name}.zh.srt"
   if [ ! -f "$zh_srt_name" ]
   then
     # .zh.srt doesn't exist, print the file name
